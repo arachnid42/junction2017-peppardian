dfe13bc154d4e1dd0b1f1b9ae29e7d282165ffbb
vsmysle <kyrylo.voronchenko@gmail.com>
2017-11-26 06:51:16 +0200

    added awareness

--- backend/test.py
+++ backend/test.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python2
 #
-
 from naoqi import ALProxy
+
+awareness = ALProxy('ALBasicAwareness')
+awareness.setEngagementMode('FullyEngaged')
+awareness.startAwareness()
 
 tts = ALProxy("ALTextToSpeech", "192.168.4.102", 9559)
 tts.say("Give it to me")
+
